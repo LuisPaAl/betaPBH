e7c47c81a413dcbfa26782a3be3145ca7849556b
Tadeo D. Aguilar <76193379+TadeoDGAguilar@users.noreply.github.com>
2023-05-14 02:43:26 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -59,7 +59,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_logo = "img/Beta_img.png"
 
-html_theme = 'renku'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
